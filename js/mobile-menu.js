da8221de03e4df7d1364bdc6a3c2184b2738c3e6
NIDIARAMIREZREYES <nidiaramirezr1109@gmail.com>
2023-06-06 21:34:00 -0500

    diseño

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,23 @@
 const menuButton = document.getElementById('menu-button');
 const menuList = document.getElementById('menu-list');
+const closeButton = document.getElementById('close-button');
 
 menuButton.addEventListener('click', () => {
-  menuList.classList.toggle('show');
+  menuList.classList.add('show');
+  menuButton.style.display = 'none';
+  closeButton.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  menuList.classList.remove('show');
+  closeButton.style.display = 'none';
+  menuButton.style.display = 'block';
 });
 
 window.addEventListener('resize', () => {
   if (window.innerWidth > 480) {
     menuList.classList.remove('show');
+    closeButton.style.display = 'none';
+    menuButton.style.display = 'none';
   }
 });
